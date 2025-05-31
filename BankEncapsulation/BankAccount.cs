2ea0c4caccc6487b2bb2ff17b8573d60bb3e32b5
Kevin Clark <kevinclarkengineer@gmail.com>
2025-05-30 23:35:00 -0400

    project completed

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double _balance; 
+    
+        public double currentBalance 
+        { 
+            get { return _balance;}
+            private set {_balance = value;}
+        }
+
+    public void Deposit(double amount)
+        {
+            Console.WriteLine($"Adding to Account Balance {amount:C} Now");
+
+            currentBalance += amount;
+
+        }
+
+        public double GetBalance()
+        {
+            return currentBalance;
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace BankEncapsulation
+﻿using System.Diagnostics;
+
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount ba = new BankAccount();
+
+            Console.WriteLine("How much currency would you like to deposit?"); 
+
+            double amountToDeposit = double.Parse(Console.ReadLine());
+
+            ba.Deposit(amountToDeposit);
+
+            double userBalance = ba.GetBalance();
+
+            Console.WriteLine($"You have added {userBalance:c} to your account.");
+
+            
+                Console.WriteLine("Would you like to make another deposit? type y for yes or n for no.");
+
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "y")
+                {
+                    Process.Start(Environment.ProcessPath);
+                }
+
+                else
+            {
+                Console.WriteLine("Thank you, come again soon!");
+            }
+           
         }
     }
 }
